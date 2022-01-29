a94ad9cb45aef5fe2446f84a47b4850fe766e0ec
I194 <i1948374@yandex.ru>
2022-01-29 11:29:16 +0300

    created react app

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.mainCointainer}>
+      Hey
+    </div>
+  );
+}
+
+export default App;
