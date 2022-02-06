4ca57b0c6a7ca26dcad3ff834b7de92a2c051cea
I194 <i1948374@yandex.ru>
2022-02-06 15:26:49 +0300

    added metadata table for DataTablePMD

--- src/components/Main/DataTablePMD/DataTablePMD.tsx
+++ src/components/Main/DataTablePMD/DataTablePMD.tsx
@@ -9,9 +9,50 @@ import { setInputFiles } from "../../../services/reducers/files";
 import { getDirectionalData, IDirData, IPmdData } from "../../../utils/files/fileManipulations";
 import { DataGrid, GridColDef, GridToolbar } from '@mui/x-data-grid';
 
+interface IMetaDataTablePMD {
+  data: IPmdData['metadata'];
+};
+
+const MetaDataTablePMD: FC<IMetaDataTablePMD> = ({ data }) => {
+
+  const columns: GridColDef[] = [
+    { field: 'name', headerName: 'Name', type: 'string' },
+    { field: 'a', headerName: 'Core Azimuth', type: 'string' },
+    { field: 'b', headerName: 'Core Dip', type: 'number' },
+    { field: 's', headerName: 'Bedding Strike', type: 'number' },
+    { field: 'd', headerName: 'Bedding Dip', type: 'number' },
+    { field: 'v', headerName: 'Volume', type: 'number' }
+  ];
+
+  columns.forEach((col) => {
+    col.align = 'center';
+    col.headerAlign = 'center';
+    col.hideSortIcons = true;
+    col.disableColumnMenu = true;
+  })
+
+  const rows = [{...data, id: 0}];
+
+  return (
+    <DataGrid 
+      rows={rows} 
+      columns={columns} 
+      sx={{
+        color: 'white',
+        maxHeight: '78px'
+      }}
+      hideFooter={true}
+      autoHeight={true}
+      density={'compact'}
+      disableSelectionOnClick={true}
+    />
+  )
+
+}
+
 interface IDataTablePMD {
   data: IPmdData;
-}
+};
 
 const DataTablePMD: FC<IDataTablePMD> = ({ data }) => {
 
@@ -49,18 +90,23 @@ const DataTablePMD: FC<IDataTablePMD> = ({ data }) => {
   });
 
   return (
-    <DataGrid 
-      rows={rows} 
-      columns={columns} 
-      sx={{
-        color: 'white'
-      }}
-      checkboxSelection
-      components={{
-        Toolbar: GridToolbar,
-      }}
-      hideFooter={true}
-    />
+    <div className={styles.dataTable}>
+      <MetaDataTablePMD 
+        data={data.metadata}
+      />
+      <DataGrid 
+        rows={rows} 
+        columns={columns} 
+        sx={{
+          color: 'white'
+        }}
+        checkboxSelection
+        components={{
+          Toolbar: GridToolbar,
+        }}
+        hideFooter={true}
+      />
+    </div>
   )
 }
 
