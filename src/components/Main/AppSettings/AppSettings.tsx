bdbd44b39025ce192c1be8bf9f61ce70d274d30f
I194 <i1948374@yandex.ru>
2022-02-06 09:34:03 +0300

    added availableFormats for input files

--- src/components/Main/AppSettings/AppSettings.tsx
+++ src/components/Main/AppSettings/AppSettings.tsx
@@ -5,7 +5,8 @@ import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
 import UploadFileOutlinedIcon from '@mui/icons-material/UploadFileOutlined';
 import { useAppDispatch } from "../../../services/store/hooks";
-import { setInputFiles } from "../../../services/reducers/files";
+import { setDirStatFiles, setInputFiles, setTreatmentFiles } from "../../../services/reducers/files";
+import { useLocation } from "react-router-dom";
 
 interface IAppSettings {
 
@@ -14,11 +15,22 @@ interface IAppSettings {
 const AppSettings: FC<IAppSettings> = () => {
 
   const dispatch = useAppDispatch();
+  const location = useLocation();
+
+  const currentPage = location.pathname.slice(1, location.pathname.length);
+  const availableFormats = {
+    pca: ['.pmd', '.csv', '.xlsx'],
+    dir: ['.dir', '.pmm', '.csv', 'xlsx'], 
+  };
 
   const handleFileUpload = (event: any) => {
     const files = Array.from(event.currentTarget.files);
+    if (currentPage === 'pca') dispatch(setTreatmentFiles(files));
+    if (currentPage === 'dir') dispatch(setDirStatFiles(files));
     dispatch(setInputFiles(files));
-  }
+  };
+
+  if (currentPage !== 'pca' && currentPage !== 'dir') return null; 
 
   return (
     <div className={styles.buttons}>
@@ -55,7 +67,7 @@ const AppSettings: FC<IAppSettings> = () => {
           type={'file'}  
           inputProps={{
             multiple: true,
-            accept: '.dir, .pmm, .pmd, .csv, .xlsx'
+            accept: availableFormats[currentPage].join(', '),
           }}
           disableUnderline={true}
           sx={{display: 'none'}}
