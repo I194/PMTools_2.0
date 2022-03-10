5f5ab664571af19085c7ecfe1542ea5ca9547960
I194 <i1948374@yandex.ru>
2022-03-10 23:05:52 +0300

    hotfix: dirpage styles path changed

--- src/pages/DIRPage/DIRPage.tsx
+++ src/pages/DIRPage/DIRPage.tsx
@@ -4,33 +4,7 @@ import styles from './DIRPage.module.scss';
 const DIRPage: FC = ({}) => {
   return (
     <>
-      <div className={styles.instruments}>
-        <div className={styles.dataSettings}>
-
-        </div>
-      </div>
-      <div className={styles.data}>
-        <div className={styles.tables}>
-          <div className={styles.tableSmall}>
-
-          </div>
-          <div className={styles.tableLarge}>
-          </div>
-        </div>
-        <div className={styles.graphs}>
-          <div className={styles.graphLarge}>
-
-          </div>
-          <div className={styles.column}>
-            <div className={styles.graphSmall}>
-
-            </div>
-            <div className={styles.graphSmall}>
-
-            </div>
-          </div>
-        </div>
-      </div>
+      directional statistics will be added soon!
     </>
   )
 }
