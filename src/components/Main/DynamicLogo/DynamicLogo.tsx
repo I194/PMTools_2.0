b44179470bdad03f7757f5b8e9650d8c4c5ed33a
Ivan Efremov <49840874+I194@users.noreply.github.com>
2022-04-29 22:09:52 +0300

    Main page with controllable sphere (#3)
    * added and initialized three.js
    
    * added sphere with contorls and net on it (wireframe)

--- src/components/Main/DynamicLogo/DynamicLogo.tsx
+++ src/components/Main/DynamicLogo/DynamicLogo.tsx
@@ -3,25 +3,36 @@ import styles from './DynamicLogo.module.scss';
 import * as THREE from 'three'
 
 import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls } from '@react-three/drei';
  
-function Box(props: JSX.IntrinsicElements['mesh']) {
-  const ref = useRef<THREE.Mesh>(null!)
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+const Sphere = (props: JSX.IntrinsicElements['mesh']) => {
+
+  const ref = useRef<THREE.Mesh>(null!);
+
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+
   return (
     <mesh
       {...props}
       ref={ref}
       scale={clicked ? 2 : 1}
-      onClick={(event) => click(!clicked)}
+      // onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? '#ce93d8' : '#90caf9'} />
+      <sphereGeometry args={[2, 32, 16]}/>
+      <meshLambertMaterial color={hovered ? '#ce93d8' : '#90caf9'} wireframe />
+      <OrbitControls 
+        minDistance={4}
+        maxDistance={8}
+      />
     </mesh>
-  )
+  );
 }
+
+
 const DynamicLogo = () => {
   const [color, colorChange] = useState("blue"); // Состояние отвечает за цвет квадрата
  
@@ -35,8 +46,7 @@ const DynamicLogo = () => {
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box position={[-2, 0, 0]} />
-      <Box position={[2, 0, 0]} />
+      <Sphere position={[0, 0, 0]} />
     </Canvas>
   );
 };
