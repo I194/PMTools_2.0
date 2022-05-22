37ec6de3032367b5e759ca6a9d64e1e1e47dcddd
I194 <i1948374@yandex.ru>
2022-05-22 17:04:47 +0300

    totally fixed vgp calculation

--- src/utils/statistics/calculation/calculateVGP.ts
+++ src/utils/statistics/calculation/calculateVGP.ts
@@ -9,22 +9,32 @@ const calculateVGP = (
 ) => {
 
   const direction = new Direction(declination, inclination, 1);
-  const p = 0.5 * Math.PI - Math.atan(Math.tan(inclination) / 2);
-  const poleLatitude = Math.asin(Math.sin(siteLatitude) * Math.cos(p) + Math.cos(siteLatitude) * Math.sin(p) * Math.cos(declination)) * Direction.RADIANS;
-  const beta = Math.asin((Math.sin(p) * Math.sin(declination) / Math.cos(poleLatitude))) * Direction.RADIANS;
-
-  let poleLongitude = siteLongitude + beta;
-  if ((Math.cos(p) - Math.sin(poleLatitude) * Math.sin(siteLatitude)) < 0) {
-    poleLongitude = siteLongitude + Math.PI - beta;
+  const paleoLatitude = direction.paleoLatitude;
+  const [declinationRad, inclinationRad, siteLatitudeRad, siteLongitudeRad, paleoLatitudeRad] = [
+    declination * Math.PI / 180,
+    inclination * Math.PI / 180,
+    siteLatitude * Math.PI / 180,
+    siteLongitude * Math.PI / 180,
+    paleoLatitude * Math.PI / 180
+  ];
+  // const p = 0.5 * Math.PI - Math.atan(Math.tan(inclination) / 2);
+  // const poleLatitude = Math.asin(Math.sin(siteLatitude) * Math.cos(p) + Math.cos(siteLatitude) * Math.sin(p) * Math.cos(declination)) * Direction.RADIANS;
+  // const beta = Math.asin((Math.sin(p) * Math.sin(declination) / Math.cos(poleLatitude))) * Direction.RADIANS;
+  const poleLatitudeRad = Math.asin(Math.sin(siteLatitudeRad) * Math.sin(paleoLatitudeRad) + Math.cos(siteLatitudeRad) * Math.cos(paleoLatitudeRad) * Math.cos(declinationRad));
+  const poleLatitude = poleLatitudeRad * Direction.RADIANS;
+  const psiRad = Math.asin(Math.cos(paleoLatitudeRad) * Math.sin(declinationRad) / Math.cos(poleLatitudeRad));
+  const psi = psiRad * Direction.RADIANS;
+  let poleLongitude = psi + siteLongitude;
+  if ((Math.sin(paleoLatitudeRad) < Math.sin(siteLatitudeRad) * Math.sin(poleLatitudeRad))) {
+    poleLongitude = 180 - psi + siteLongitude;
   }
   // Bind the plate longitude between [0, 360]
   if (poleLongitude < 0) {
-    poleLongitude += 2 * Math.PI;
+    poleLongitude += 360;
   }
 
-  const paleoLatitude = direction.paleoLatitude;
-  const dp = 2 * a95 / (1  + 3 * Math.pow(Math.cos(paleoLatitude), 2));
-  const dm = a95 * Math.cos(paleoLatitude) / Math.cos(inclination);
+  const dp = 2 * a95 / (1  + 3 * Math.pow(Math.sin(inclinationRad), 2));
+  const dm = a95 * Math.cos(paleoLatitudeRad) / Math.cos(inclinationRad);
 
   const res: {
     poleLatitude: number,
