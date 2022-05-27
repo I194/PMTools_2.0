404d0062819e54ef995b0e3084e4a2cc3dca6f43
I194 <i1948374@yandex.ru>
2022-05-27 08:24:02 +0300

    images compressed

--- src/components/MainPage/FeaturesCards/assets/index.ts
+++ src/components/MainPage/FeaturesCards/assets/index.ts
@@ -1,17 +1,17 @@
-import pca from './pca.png';
-import pcaDark from './pcaDark.png';
-import dir from './dir.png';
-import dirDark from './dirDark.png';
-import vgp from './vgp.png';
-import vgpDark from './vgpDark.png';
-import selection from './selection.png';
-import selectionDark from './selectionDark.png';
-import exportData from './exportData.png';
-import exportDataDark from './exportDataDark.png';
-import exportGraphs from './exportGraphs.png';
-import exportGraphsDark from './exportGraphsDark.png';
-import darkTheme from './darkTheme.png';
-import darkThemeDark from './darkThemeDark.png';
+import pca from './pca.jpg';
+import pcaDark from './pcaDark.jpg';
+import dir from './dir.jpg';
+import dirDark from './dirDark.jpg';
+import vgp from './vgp.jpg';
+import vgpDark from './vgpDark.jpg';
+import selection from './selection.jpg';
+import selectionDark from './selectionDark.jpg';
+import exportData from './exportData.jpg';
+import exportDataDark from './exportDataDark.jpg';
+import exportGraphs from './exportGraphs.jpg';
+import exportGraphsDark from './exportGraphsDark.jpg';
+import darkTheme from './darkTheme.jpg';
+import darkThemeDark from './darkThemeDark.jpg';
 
 export {
   pca,
