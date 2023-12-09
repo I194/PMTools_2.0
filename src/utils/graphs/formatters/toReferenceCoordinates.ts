6c0789beaff35fe46788602fc5395c2a965e2c24
i1948374 <i1948374@gmail.com>
2023-12-09 14:46:23 +0300

    [Исправление ошибок] Очередной фикс toReferenceCoordinates, связанный с переходом на localStorage

--- src/utils/graphs/formatters/toReferenceCoordinates.ts
+++ src/utils/graphs/formatters/toReferenceCoordinates.ts
@@ -9,13 +9,14 @@ const toReferenceCoordinates = (
 ) => {
   // Function inReferenceCoordinates
   // rotates the coordinates to the reference coordinates
-  
-  if (reference == "specimen") return coordinates;
+  const {x, y, z} = coordinates;
+  let coordsHelper = new Coordinates(x, y, z);
+
+  if (reference == "specimen") return coordsHelper;
 
   // Иногда по ошибке можно передать вместо coordinates просто объект {x, y, z} и он пройдет тайпчек пропсов и тут это вызовет ошибки ниже
   // И потому, чтобы такие ошибки не допустить, приходится создавать coordsHelper, у которого наверняка определены все методы Coordinates
-  const {x, y, z} = coordinates;
-  let coordsHelper = new Coordinates(x, y, z);
+
   // Do the geographic correction
   coordsHelper = coordsHelper.rotateTo(metadata.a, metadata.b);
 
